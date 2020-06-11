e5a0142e2b8f7936fef6d3d9380efcfc013d1b12
Ikramuddin21 <shaonakhand2122@gmail.com>
2020-06-11 12:21:19 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const dbUser = 'dbUser';
+const pass = 'EgM64zuLTAwVgJcF';
+const uri = "mongodb+srv://dbUser:EgM64zuLTAwVgJcF@cluster0-ilfiw.mongodb.net/onlineStore?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri, { useNewUrlParser: true });
+const users = ['Asad', 'Moin', 'Sabed', 'Susmita', 'Sohana', 'Sabana'];
+
+app.get('/', (req, res) =>{
+    const fruit = {
+        product: 'ada',
+        price: 220
+    }
+    res.send(fruit);
+});
+
+app.get('/users/:id', (req, res) =>{
+    const id = req.params.id;
+    const name = users[id];
+    res.send({id, name});
+});
+
+//post
+app.post('/addProduct', (req, res) => {
+    const product = req.body;
+    console.log(product);
+
+    client.connect(err => {
+        const collection = client.db("onlineStore").collection("products");
+        collection.insertOne(product, (err, res)=>{
+            console.log('Successfully inserted', res)
+            // res.send(product);
+        });
+        client.close();
+      });
+});
+app.listen(4000, () => console.log('Listening to port 4000'));
